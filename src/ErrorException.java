8d6433f51fbe0a921ba681356215126dd082ff00
e185745 <daxter0306@gmail.com>
2023-01-04 15:54:59 +0900

    finished step2

--- src/ErrorException.java
+++ src/ErrorException.java
@@ -1,6 +1,12 @@
 public class ErrorException{
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }
+        catch(NullPointerException exception){
+            System.out.println(exception.getMessage());
+            System.out.println("NullPointerExceptionが発生しました");
+        }
     }
 }
