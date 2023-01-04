f2980b7403c3de276ad592febffdbc7482824134
e185745 <daxter0306@gmail.com>
2023-01-04 16:03:36 +0900

    finished step3

--- src/ErrorException.java
+++ src/ErrorException.java
@@ -1,12 +1,6 @@
 public class ErrorException{
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try{
-            System.out.println(str.length());
-        }
-        catch(NullPointerException exception){
-            System.out.println(exception.getMessage());
-            System.out.println("NullPointerExceptionが発生しました");
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
